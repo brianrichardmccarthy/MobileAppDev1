33e02674eb2b37018b1cd8eb14f1211c699231c7
Brian McCarthy <brianmccarthy1@outlook.ie>
2018-10-24 10:46:03 +0100

    00 Overview Setup

---
+++ Placemark/app/src/main/java/io/github/brianrichardmccarthy/placemark/PlacemarkActivity.kt
@@ -0,0 +1,12 @@
+package io.github.brianrichardmccarthy.placemark
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class PlacemarkActivity : AppCompatActivity() {
+
+  override fun onCreate(savedInstanceState: Bundle?) {
+    super.onCreate(savedInstanceState)
+    setContentView(R.layout.activity_placemark)
+  }
+}
